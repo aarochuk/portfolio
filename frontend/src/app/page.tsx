5bb334cec57b67e69315329b6f5069f4e397b65a
aarochuk <andrewarochukwu@gmail.com>
2025-02-04 18:29:35 -0500

    footer added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,11 @@
 import styles from "./page.module.css";
 import Image from 'next/image';
-import sun from '/public/icons/sun.png';
 import github from '/public/icons/github.png';
 import linkedin from '/public/icons/linkedin.png';
 import codepen from '/public/icons/codepen.png';
 import lock from '/public/icons/padlock.png';
+import walking from '/public/icons/walking.png';
+import timemachine from '/public/icons/timemachine.png';
 
 export default function Home() {
   return (
@@ -12,7 +13,6 @@ export default function Home() {
       <div className={styles.main}>
         <div className={styles.staticHalf}>
           <div className={styles.icons}>
-            <Image src={sun} alt="Sun" width={30} height={30}/>
             <a href="https://github.com/aarochuk" target="_blank">
               <Image src={github} alt="github" width={30} height={30}/>
             </a>
@@ -22,14 +22,14 @@ export default function Home() {
             <a href="https://codepen.io/odinahand" target="_blank">
               <Image src={codepen} alt="github" width={30} height={30}/>
             </a>
-            <Image src={lock} alt="github" width={30} height={30}/>
+        <Image src={walking} alt="github" width={30} height={30}/>
           </div>
           <div className={styles.sections}>
             <h2 className={styles.firstName}>Andrew</h2>
             <div className={styles.lastName}>
               <h2>A</h2><h3>ROCHUKWU</h3>
             </div>
-            <p className={styles.underName}>Tryna become a 100x software engineer or make it to the nba at 25.</p>
+            <p className={styles.underName}>Tryna become a 100x software engineer or make it to the nba at 26.</p>
             <ul>
               <li>about</li>
               <li>experience</li>
@@ -43,7 +43,13 @@ export default function Home() {
         </div>
       </div>
       <div className={styles.footer}>
-      
+        <p>&copy; Andrew Arochukwu 2025</p>
+        <div className={styles.leftFooter}>
+          <Image src={timemachine} alt="timemachine" width={40} height={40}/>
+          <select>
+            <option value="english">English</option>
+          </select>
+        </div>
       </div>
     </div>
   );
