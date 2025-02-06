c4fc37f5b3126d7475854c6b6e9b36df36139475
aarochuk <andrewarochukwu@gmail.com>
2025-02-06 14:14:36 -0500

    cleaned some stuff up

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,8 +3,6 @@ import Image from 'next/image';
 import github from '/public/icons/github.png';
 import linkedin from '/public/icons/linkedin.png';
 import codepen from '/public/icons/codepen.png';
-import lock from '/public/icons/padlock.png';
-import walking from '/public/icons/walking.png';
 import timemachine from '/public/icons/timemachine.png';
 import resume from '/public/files/resume.pdf';
 
@@ -23,7 +21,6 @@ export default function Home() {
             <a href="https://codepen.io/odinahand" target="_blank">
               <Image src={codepen} alt="github" width={30} height={30}/>
             </a>
-        <Image src={walking} alt="github" width={30} height={30}/>
           </div>
           <div className={styles.sections}>
             <h2 className={styles.firstName}>Andrew</h2>
@@ -32,7 +29,6 @@ export default function Home() {
             </div>
             <p className={styles.underName}>Tryna become a 100x software engineer or make it to the nba at 26.</p>
             <ul>
-              <li>about</li>
               <li>experience</li>
               <li>projects</li>
               <li>blog</li>
